9d3f65e855d666c4376d03151f0dc181d3bb416f
ValerieDoleac <114406228+ValerieDoleac@users.noreply.github.com>
2023-03-28 11:49:58 +0200

    28_03_2023

---
+++ image/style.js
@@ -0,0 +1,4 @@
+const menuHamburger = document.querySelector(".menu")
+const navLinks = document.querySelector(".nav-links")
+
+menuHamburger.addEventListener('click',()=>{navLinks.classList.toggle('mobile-menu')})
